7d789929c1a4805fd4e652aa5bed36df24e9d29a
Owen Trueblood <hi@owentrueblood.com>
2017-10-17 18:03:09 -0400

    Unflip x and y

--- Processing/maskDraw/Plotter.java
+++ Processing/maskDraw/Plotter.java
@@ -207,7 +207,7 @@ class Plotter implements Runnable {
     public int[] getData() {
       // Negate and switch X and Y so the origin is in the upper left relative
       // to the controls
-      return makePacket('m', y, x, z);
+      return makePacket('m', x, y, z);
     }
 
     public boolean needsSync() {
